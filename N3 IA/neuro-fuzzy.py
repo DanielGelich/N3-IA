3d1ad9c7c0b9160b3cd438c75c79cb43ccdfeb14
Daniel <82720759+DanielGelich@users.noreply.github.com>
2023-12-07 19:51:47 -0300

    neuro-fuzzy.py

--- N3 IA/neuro-fuzzy.py
+++ N3 IA/neuro-fuzzy.py
@@ -12,7 +12,7 @@ vel_linear = ctrl.Antecedent(np.arange(-10, 11, 1), 'vel_linear')
 
 acao = ctrl.Consequent(np.arange(-10, 11, 1), 'acao')
 
-# O ângulo
+# angulo
 angulo['N'] = fuzz.trimf(angulo.universe, [-180, -180, 0])
 angulo['Z'] = fuzz.trimf(angulo.universe, [-15, 0, 15])
 angulo['P'] = fuzz.trimf(angulo.universe, [0, 180, 180])
@@ -22,7 +22,7 @@ vel_angular['N'] = fuzz.trimf(vel_angular.universe, [-10, -10, 0])
 vel_angular['Z'] = fuzz.trimf(vel_angular.universe, [-2, 0, 2])
 vel_angular['P'] = fuzz.trimf(vel_angular.universe, [0, 10, 10])
 
-# A posição
+# posição
 posicao['N'] = fuzz.trimf(posicao.universe, [-10, -10, 0])
 posicao['Z'] = fuzz.trimf(posicao.universe, [-2, 0, 2])
 posicao['P'] = fuzz.trimf(posicao.universe, [0, 10, 10])
@@ -36,8 +36,7 @@ acao['N'] = fuzz.trimf(acao.universe, [-10, -10, 0])
 acao['Z'] = fuzz.trimf(acao.universe, [-2, 0, 2])
 acao['P'] = fuzz.trimf(acao.universe, [0, 10, 10])
 
-# Regras !
-
+# regras !
 regra_1 = ctrl.Rule(antecedent=(angulo['N'] & vel_angular['N'] & posicao['N'] & vel_linear['N']), consequent=acao['P'])
                    
 regra_2 = ctrl.Rule(antecedent=(angulo['N'] & vel_angular['Z'] & posicao['N'] & vel_linear['Z']), consequent=acao['N'])
